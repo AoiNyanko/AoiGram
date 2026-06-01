b28b51a9a07d2e2a4a38d18ddf891b7871bbdc28
QuanWenG <154224091+QuanWenG@users.noreply.github.com>
2026-06-01 14:39:58 +0800

    Configurable COLORADO package and aoigram tasks
    Make the native COLORADO package name configurable from Gradle and add aoigram build tasks. Pass -DCOLORADO_PACKAGE_NAME to CMake based on requested Gradle task, generate colorado_package_name.h from a template and add the generated include directory to the colorado target (defaults to "tw.nekomimi.nekogram" if not provided). Update colorado.h to prefer the generated macro. Modify TMessagesProj_App to switch applicationId when aoigram build tasks are requested and register aoigram-release/aoigram-debug/aoigram-play helper tasks that depend on the corresponding assemble tasks.

--- TMessagesProj/jni/colorado/colorado.h
+++ TMessagesProj/jni/colorado/colorado.h
@@ -3,7 +3,15 @@
 #ifdef NDEBUG
 #define LOG_DISABLED
 #endif
-#define PACKAGE_NAME "tw.nekomimi.nekogram"_iobfs.c_str()
+#ifdef __has_include
+#if __has_include("colorado_package_name.h")
+#include "colorado_package_name.h"
+#endif
+#endif
+#ifndef COLORADO_PACKAGE_NAME_OBFS
+#define COLORADO_PACKAGE_NAME_OBFS "tw.nekomimi.nekogram"_iobfs.c_str()
+#endif
+#define PACKAGE_NAME COLORADO_PACKAGE_NAME_OBFS
 #define CERT_HASH 0x693cc8c5
 #define CERT_SIZE 0x2d7
 
